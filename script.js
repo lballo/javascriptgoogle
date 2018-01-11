513fa44b8b489ef711156eb0d2065fe81271ce33
franck ballo <laura.ballo1993@gmail.com>
2018-01-11 17:50:01 +0100

    projet final

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function afficherCacher(){
+	document.getElementById("button").style.display = "none";
+
+  	var MaVariable = document.getElementById("afficher");
+
+	if(MaVariable.style.display == "none")
+    {
+    	MaVariable.style.display = "block";
+    }
+    else
+    {
+        MaVariable.style.display="block";
+    }
+}
+
+
+
+setTimeout(function(){
+	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+}, 10000);
+
+
